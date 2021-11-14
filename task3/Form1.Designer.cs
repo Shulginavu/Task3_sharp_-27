060c7a5c3ef7078642dcf1b758e533fd4ebc7066
Shulginavu <66249921+Shulginavu@users.noreply.github.com>
2021-11-14 22:53:01 +0300

    Add files via upload

---
+++ task3/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(345, 108);
+            this.listBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить из файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Поменять состояние";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Сумма +";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Сумма -";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Номер",
+            "Сумма",
+            "Состояние"});
+            this.comboBox1.Location = new System.Drawing.Point(136, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Сортировка";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Номер",
+            "Банк",
+            "Сумма",
+            "Состояние"});
+            this.comboBox2.Location = new System.Drawing.Point(136, 215);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(103, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 213);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Поиск";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(245, 216);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(112, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 251);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(345, 108);
+            this.listBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 398);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ task3/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        private List<Account> accounts = new List<Account>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            listBox1.Items.Clear();
+            string[] fileText = System.IO.File.ReadAllLines(filename);
+            foreach (string line in fileText) {
+                string[] sline = line.Split(' ');
+               
+                listBox1.Items.Add(new Account(Convert.ToInt32(sline[0]), sline[1], Convert.ToInt32(sline[2]), Convert.ToInt32(sline[3])).toString());
+                accounts.Add(new Account(Convert.ToInt32(sline[0]), sline[1], Convert.ToInt32(sline[2]), Convert.ToInt32(sline[3])));
+            }
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                return; 
+            }
+            if (accounts[listBox1.SelectedIndex].isActive == 1)
+            {
+                accounts[listBox1.SelectedIndex].isActive = 0;
+            }
+            else
+            {
+                accounts[listBox1.SelectedIndex].isActive = 1;
+            }
+            listBox1.Items.Clear();
+            foreach (Account a in accounts)
+            {
+                listBox1.Items.Add(a.toString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            foreach (Account a in accounts)
+            {
+                if (a.balance >= 0) sum += a.balance;
+            }
+            textBox1.Text = Convert.ToString(sum);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            foreach (Account a in accounts)
+            {
+                if (a.balance <= 0) sum += a.balance;
+            }
+            textBox2.Text = Convert.ToString(sum);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "") return;
+
+            if (comboBox1.Text == "Номер") {
+                accounts = accounts.OrderBy(b => b.number).ToList();
+            }
+            if (comboBox1.Text == "Сумма")
+            {
+                accounts = accounts.OrderBy(b => b.balance).ToList();
+            }
+            if (comboBox1.Text == "Состояние")
+            {
+                accounts = accounts.OrderByDescending(b => b.isActive).ToList();
+            }
+
+            listBox1.Items.Clear();
+            foreach (Account a in accounts)
+            {
+                listBox1.Items.Add(a.toString());
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            if (comboBox2.Text == "") return;
+
+            if (comboBox2.Text == "Номер")
+            {
+                foreach (Account a in accounts)
+                {
+                    if(a.number == Convert.ToInt32(textBox3.Text)) listBox2.Items.Add(a.toString());
+                }
+            }
+            if (comboBox2.Text == "Сумма")
+            {
+                foreach (Account a in accounts)
+                {
+                    if (a.balance == Convert.ToInt32(textBox3.Text)) listBox2.Items.Add(a.toString());
+                }
+            }
+            if (comboBox2.Text == "Состояние")
+            {
+                foreach (Account a in accounts)
+                {
+                    string isActiveToStr = (a.isActive == 1) ? "Активен" : "Заблокирован";
+                    if (isActiveToStr == textBox3.Text) listBox2.Items.Add(a.toString());
+                }
+            }
+            if (comboBox2.Text == "Банк")
+            {
+                foreach (Account a in accounts)
+                {
+                    if (a.bankName == textBox3.Text) listBox2.Items.Add(a.toString());
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Account
+    {
+        public Account(int number, string bankName, int balance, int isActive)
+        {
+            this.number = number;
+            this.bankName = bankName;
+            this.balance = balance;
+            this.isActive = isActive;
+        }
+        public int number { get; set; }
+        public string bankName { get; set; }
+        public int balance { get; set; }
+        public int isActive { get; set; }
+        public string toString() 
+        {
+            string isActiveToStr = (isActive == 1) ? "Активен" : "Заблокирован";
+            return number + " " + bankName + " " + balance + " " + isActiveToStr;
+        }
+
+    }
+}
